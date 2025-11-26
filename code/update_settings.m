77c2a682a5f4f0dbd826e2e13b2b50a2c884ebc5
InesVandekerckhove <ines.vandekerckhove@kuleuven.be>
2025-11-26 11:01:26 +0100

    Deleted passive shift for ankle muscles from update_settings

--- code/update_settings.m
+++ code/update_settings.m
@@ -3,8 +3,5 @@ function[S] = update_settings(S)
 % Full gait cycle simulations instead of Half gait cycle (default) simulations
 S.misc.gaitmotion_type = 'FullGaitCycle';
 
-% Baseline setting for 2D model - shift the passive force-length curve of the ankle muscles to
-% 0.9 of normalized muscle length 
-S.subject.muscle_pass_stiff_shift = {{'tib_', 'gastroc_', 'soleus_'},0.9};
 
 end
